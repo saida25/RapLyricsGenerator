9869d247bd71839b8761e3f2af2bf1683e604479
Saida Yengui <saida.yengui@gmail.com>
2025-08-21 20:03:23 +0100

    add templates

--- lyrics_generator/urls.py
+++ lyrics_generator/urls.py
@@ -16,7 +16,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from lyrics_generator import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
+    path('artists/', views.artist_list, name='artist_list'),
+    path('artists/add/', views.add_artist, name='add_artist'),
+    path('lyrics/', views.lyrics_list, name='lyrics_list'),
+    path('lyrics/add/', views.add_lyrics, name='add_lyrics'),
+    path('train/', views.train_model, name='train_model'),
+    path('generate/', views.generate_lyrics, name='generate_lyrics'),
+    path('generated/', views.generated_lyrics_list, name='generated_lyrics_list'),
 ]
